ed3deaa60a56c19be84cd992b711c91a82bf429d
Mikhail Shpakov <mikhail.n.shpakov@gmail.com>
2021-06-09 23:10:02 +0300

    Создан каркас урока

---
+++ arrays.js
@@ -0,0 +1,43 @@
+const simpleArray = [1, 4, 5, 2, 9, 8, 6]
+
+const complexArray = [
+  { name: 'Jane', age: 19, city: 'Saint-Petersburg', company: 'Timeweb', },
+  { name: 'Serg', age: 22, city: 'Moscow', company: 'Apple' },
+  { name: 'Anna', age: 18, city: 'Rostov', company: 'Google' },
+]
+
+// массивы, как объекты
+
+// push, pop
+
+// shift, unshift
+
+// splice
+
+// slice
+
+// concat
+
+// forEach
+
+// indexOf, lastIndexOf
+
+// includes
+
+// find, findIndex
+
+// filter
+
+// map
+
+// sort
+
+// reverse
+
+// split, join
+
+// reduce, reduceRight
+
+// комбинирование методов
+
+// typeof, Array.isArray
